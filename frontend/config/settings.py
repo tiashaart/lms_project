c915d2ae82c6f03fbe178469a9953ce62b695912
Shahida Shajahan <kookiezeha5@gmail.com>
2026-08-05 19:21:20 +0530

    Update settings.py

--- frontend/config/settings.py
+++ frontend/config/settings.py
@@ -58,13 +58,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": postgres,
-        "USER": postgres,
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": db.gglhnpdjdqevrwaghfvj.supabase.co,
+        "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
